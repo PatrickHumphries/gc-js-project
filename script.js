de979f69d01e90d6e4e462bc5d7f21ef721ee5ce
Nick Maxwell <NMaxwell93@gmail.com>
2021-04-20 10:44:24 -0500

    JS start- prompt total displaying

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+{
+    let totalBudget = prompt("What is your weekly budget?")
+    const budgetTotalDisplay = document.getElementById("totalBudget");
+    budgetTotalDisplay.innerText = `$${totalBudget}`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
