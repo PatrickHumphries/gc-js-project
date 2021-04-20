64cd01301cbe3382244dc15cea0739fcdee735e5
Nick Maxwell <NMaxwell93@gmail.com>
2021-04-20 10:58:57 -0500

    budget remainder display setup

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
     //let totalBudget = prompt("What is your weekly budget?")
     const budgetTotalDisplay = document.getElementById("totalBudget");
     budgetTotalDisplay.innerText = `$${totalBudget}`;
+    const budgetRemainderDisplay = document.getElementById("remaining");
+    // budget remaining display, need var still for
+    //budgetRemainderDisplay = `$${remainderVar}`;
 
 
 
