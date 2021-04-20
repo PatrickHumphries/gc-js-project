0ce113641d7d19ce5cf8c03310e9bcc136701112
Nick Maxwell <NMaxwell93@gmail.com>
2021-04-20 12:50:54 -0500

    working on subtraction

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 "use strict";
 {
-    //let totalBudget = prompt("What is your weekly budget?")
-    const budgetTotalDisplay = document.getElementById("totalBudget");
-    budgetTotalDisplay.innerText = `$${totalBudget}`;
-    const budgetRemainderDisplay = document.getElementById("remaining");
-    // budget remaining display, need var still for
-    //budgetRemainderDisplay = `$${remainderVar}`;
+  let totalBudget = prompt("What is your weekly budget?")
+  const budgetTotalDisplay = document.getElementById("totalBudget");
+  budgetTotalDisplay.innerText = `$${totalBudget}`;
+  const budgetRemainderDisplay = document.getElementById("remaining");
+  // budget remaining display, need var still for
+  let remainderVar = totalBudget
+  budgetRemainderDisplay.innerText = `$${remainderVar}`;
+    
+  // value of inputs
+  const budgetInput = document.getElementById("subtractBtn");
 
+  budgetInput.addEventListener("click", (event) => {
+      event.preventDefault();
+      console.log("button is clicked")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      let itemName = document.getElementById("itemInput").value;
+      let itemCost = document.getElementById("itemCost").value;
+      let itemCategory = document.getElementById("categories").value;
+      
+      remainderVar -= itemCost
+  })
 }
-
-
