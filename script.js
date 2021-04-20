106a5a48ddf7aaa23ea3de41be79769640e3e2cd
Nick Maxwell <NMaxwell93@gmail.com>
2021-04-20 10:54:15 -0500

    disabled category in dropdown

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 {
-    let totalBudget = prompt("What is your weekly budget?")
+    //let totalBudget = prompt("What is your weekly budget?")
     const budgetTotalDisplay = document.getElementById("totalBudget");
     budgetTotalDisplay.innerText = `$${totalBudget}`;
 
